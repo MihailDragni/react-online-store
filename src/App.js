8ca56282ff8b783e1eb119325939d10efc3b5dd6
MihailDragni <74327586+MihailDragni@users.noreply.github.com>
2023-05-05 01:41:19 +0300

    added cart menu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      orders: [],
       items: [
         {
           id: 1,
@@ -41,24 +42,58 @@ class App extends React.Component {
           price: '25.00',
         },
         {
-          id: 4,
+          id: 5,
           title: 'Стул белый',
           img: 'chair-white.jpeg',
           desc: 'Белый стул - стильный и минималистичный элемент интерьера.',
           category: 'chairs',
           price: '49.00',
         },
+        {
+          id: 6,
+          title: 'Кресло',
+          img: 'Armchair.jpg',
+          desc: 'Мягкое кресло - уютное место для отдыха и релакса.',
+          category: 'armchair',
+          price: '249.00',
+        },
+        {
+          id: 7,
+          title: 'Стол для работы',
+          img: 'Work-table.jpg',
+          desc: 'Прочный, удобный, просторный стол для эффективной работы дома или офиса.',
+          category: 'table',
+          price: '349.00',
+        },
       ],
     }
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
-        <Header />
-        <Items items={this.state.items} />
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Items items={this.state.items} onAdd={this.addToOrder} />
         <Footer />
       </div>
     )
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter((el) => el.id !== id) })
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach((el) => {
+      if (el.id === item.id) {
+        isInArray = true
+      }
+    })
+    if (!isInArray) {
+      this.setState({ orders: [...this.state.orders, item] })
+    }
   }
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
+import Order from './Order'
 
-const Header = () => {
+const showOrders = (props) => {
+  let summa = 0
+  props.orders.forEach((el) => (summa += Number.parseFloat(el.price)))
+  return (
+    <>
+      {props.orders.map((el) => (
+        <Order onDelete={props.onDelete} key={el.id} item={el} />
+      ))}
+      <p className="summa">Сумма: {new Intl.NumberFormat().format(summa)}$</p>
+    </>
+  )
+}
+
+const showNothing = () => {
+  return (
+    <div className="empty">
+      <h3>Товаров нет</h3>
+    </div>
+  )
+}
+
+const Header = (props) => {
   let [cartOpen, setCartOpen] = useState(false)
   return (
     <header className="header">
@@ -11,7 +33,11 @@ const Header = () => {
           onClick={() => setCartOpen((cartOpen = !cartOpen))}
           className={`shop-cart-button ${cartOpen ? 'active' : ''}`}
         />
-        {cartOpen && <div className="shop-cart"></div>}
+        {cartOpen && (
+          <div className="shop-cart">
+            {props.orders.length > 0 ? showOrders(props) : showNothing()}
+          </div>
+        )}
         <ul className="header__menu menu">
           <li>Про нас</li>
           <li>Контакты</li>
